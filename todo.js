7035fae2785a9cf4b1c9c991e4bdf4713865aac6
Gözde <gozdecan2214@gmail.com>
2023-04-14 01:36:33 +0300

    To Do App

---
+++ todo.js
@@ -0,0 +1,164 @@
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
+const deleteAllButton = document.querySelector("#delete-all");
+
+document.addEventListener("DOMContentLoaded", getLocalTodos);
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("change", filterTodo);
+deleteAllButton.addEventListener("click", allTodo);
+
+function addTodo(event) {
+  event.preventDefault();
+  if (todoInput.value == "") {
+    // yeni satır
+    showAlert("danger", "Lütfen bir todo girin.");
+  } else {
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    //   ADDING TO LOCAL STORAGE
+    saveLocalTodos(todoInput.value);
+    showAlert("warning", "Todo başarıyla eklendi."); // yeni eklendi
+
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = ' <i class="fa-solid fa-circle-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fa-sharp fa-solid fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    todoList.appendChild(todoDiv);
+    todoInput.value = "";
+  }
+}
+
+function deleteCheck(e) {
+  const item = e.target;
+
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    todo.classList.add("slide");
+
+    removeLocalTodos(todo);
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
+  }
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+    showAlert("info", "Todo tamamlandı!");
+  }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "complete":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "incomplete":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
+}
+
+function saveLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+function getLocalTodos() {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.forEach(function (todo) {
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todo;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = ' <i class="fa-solid fa-circle-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fa-sharp fa-solid fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    todoList.appendChild(todoDiv);
+  });
+}
+
+function removeLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  const todoIndex = todo.children[0].innerText;
+  todos.splice(todos.indexOf(todoIndex), 1);
+  localStorage.setItem("todos", JSON.stringify(todos));
+  showAlert("success", "Todo başarıyla silindi.");
+}
+function showAlert(type, message) {
+  const alertDiv = document.createElement("div");
+  alertDiv.className = `alert alert-${type}`;
+  alertDiv.appendChild(document.createTextNode(message));
+
+  const container = document.querySelector(".todo-container");
+  const form = document.querySelector("#todo-form");
+  container.insertBefore(alertDiv, form);
+
+  setTimeout(function () {
+    document.querySelector(".alert").remove();
+  }, 1000);
+}
+
+function allTodo() {
+  const todos = document.querySelectorAll(".todo");
+  if (todos.length > 0) {
+    Array.from(todos).forEach(function (todo) {
+      todo.remove();
+    });
+    localStorage.removeItem("todos");
+    showAlert("success", "Başarılı bir şekilde silindi");
+  } else {
+    showAlert("danger", "Silmek için en az bir todo olmalıdır");
+  }
+}
